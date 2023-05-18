1ef802d6a7e0ebdb00ff733a6dc62933bbfac670
Sergei Korchagin <gamestationbase@gmail.com>
2023-05-18 23:36:20 +0300

    the final part of the code has been removed

--- src/js/app.js
+++ src/js/app.js
@@ -41,43 +41,6 @@ allColumn.forEach(item => {
     const allCards = document.querySelectorAll('.card');
     displayCloseBtns(allCards);
 
-    ///////////>
-    const internalContainer = document.querySelector('.internal-container');
-    const itemCard = internalContainer.querySelector('.card');
-
-    let actualElement;
-
-    const onMouseUp = (e) => {
-      actualElement.classList.remove('dragged');
-      actualElement = undefined;
-
-      document.body.removeEventListener('mouseup', onMouseUp);
-      document.body.removeEventListener('mouseover', onMouseOver);
-
-    }
-
-    const onMouseOver = (e) => {
-      console.log(e);
-
-      actualElement.style.top = e.clientY + 'px';
-      actualElement.style.left = e.clientX + 'px';
-    }
-
-    internalContainer.addEventListener('mousedown', (e) => {
-      e.preventDefault();
-      if (e.target.classList.contains('card')) {
-        actualElement = e.target;
-        actualElement.classList.add('dragged');
-      }
-
-      document.body.addEventListener('mouseup', onMouseUp);
-      document.body.addEventListener('mouseover', onMouseOver);
-
-    });
-
-
-
-
   });
 });
 
